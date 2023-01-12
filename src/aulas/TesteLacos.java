8ea7836b47b59323f064cffac2bf1c3c65a1bf15
Mauricio Lopes <mauricio.lopes@mercadolivre.com>
2023-01-12 12:00:08 -0300

    Exercicios 03_lacos_repeticao_java

---
+++ src/aulas/TesteLacos.java
@@ -0,0 +1,15 @@
+package aulas;
+
+public class TesteLacos {
+    public static void main(String[] args) {
+        for (int contador = 0; contador < 3; contador++) {
+            System.out.println("Test " + contador);
+        }
+
+        int contadorWhile = 0;
+        while (contadorWhile < 3) {
+            System.out.println("Isso é while papai! " + contadorWhile);
+            contadorWhile++;
+        }
+    }
+}
